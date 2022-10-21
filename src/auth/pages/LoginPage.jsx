8ec7a30cd946f8110ea2364d68a37ec2641deb9b
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-10-20 20:23:44 -0500

    Finalizar styles de Login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 
 export const LoginPage = () => {
   return (
@@ -39,12 +41,24 @@ export const LoginPage = () => {
               />
             </Grid>
 
-            <Grid container spacing={2} sx={{ mb: 2 }}>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
               <Grid item xs={12} sm={6}>
                 <Button variant="contained" fullWidth>
                   Login
                 </Button>
               </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+
+            <Grid container direction="row" justifyContent="end">
+              <Link component={RouterLink} color="inherit" to="/auth/register">
+                Crear una cuenta
+              </Link>
             </Grid>
           </Grid>
         </form>
