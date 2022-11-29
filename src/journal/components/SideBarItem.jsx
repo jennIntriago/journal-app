0d0e69c4eb5932fbf243165708736fed3fdce822
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-11-29 16:32:42 -0500

    Activar nota en menu lateral

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -7,14 +7,21 @@ import {
   ListItemText,
 } from "@mui/material";
 import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal";
 
-export const SideBarItem = ({ title, body, id }) => {
+export const SideBarItem = ({ title, body, id, date, imageUrl = [] }) => {
+  const dispatch = useDispatch();
+  const onClickNote = () => {
+    console.log("me dieron cli");
+    dispatch(setActiveNote({ title, body, id, date, imageUrl }));
+  };
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + "..." : title;
   }, [title]);
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onClickNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
