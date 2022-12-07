b32e47c653e8b7af2d75fb8a517f6dae763caeaa
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-12-07 16:06:47 -0500

    Configuracion de cludinary para borrar imagen despues de subirla en el test

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,4 +1,14 @@
+import { v2 as cloudinary } from "cloudinary";
 import { fileUpload } from "../../src/helpers/fileUpload";
+
+// Configuracion para usar SDK de cloudinary
+
+cloudinary.config({
+  cloud_name: "dry0ncppk",
+  api_key: "479872988938914",
+  api_secret: "nBTzjH1KHVlrHk7ClRcpTc5GAQk",
+  secure: true,
+});
 
 describe("Pruebas en fileUploads", () => {
   test("debe de subir el archivo correctamente a cloudidany", async () => {
@@ -16,5 +26,21 @@ describe("Pruebas en fileUploads", () => {
 
     // evaluar la url-img
     expect(typeof url).toBe("string");
+
+    // limpieza en cloudinary
+    const segments = url.split("/");
+    const imgId = segments[segments.length - 1].replace(".jpg", "");
+    const cloudResp = await cloudinary.api.delete_resources(
+      ["journal/" + imgId],
+      { resource_type: "image" }
+    );
+    console.log({ cloudResp });
+  });
+
+  test("debe retornar null", async () => {
+    const file = new File([], "foto.jpg");
+    const url = await fileUpload(file);
+    // evaluar la url-img
+    expect(url).toBe(null);
   });
 });
