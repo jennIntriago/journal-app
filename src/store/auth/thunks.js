48bd3a5b71d47e9b7fbdd90ad30f9596c6fbb01e
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-12-19 10:12:41 -0500

    Pruebas con los thunks

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -18,6 +18,7 @@ export const startGoogleSignIn = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
     const result = await signInWithGoogle();
+
     if (!result.ok) return dispatch(logout(result.errorMessage));
     dispatch(login(result));
   };
